8173e9aa7c85f3c18d80826151fd0346e41d412c
Javier Martinez <javiermarsal11@gmail.com>
2024-10-18 12:18:55 +0200

    modelo usuario añadido

--- src/main/java/org/example/modelo/Usuario.java
+++ src/main/java/org/example/modelo/Usuario.java
@@ -4,5 +4,32 @@ public class Usuario {
     private int idUsuario;
     private String nombre;
 
-    
+    public Usuario(int idUsuario, String nombre) {
+        this.idUsuario = idUsuario;
+        this.nombre = nombre;
+    }
+
+    public int getIdUsuario() {
+        return idUsuario;
+    }
+
+    public void setIdUsuario(int idUsuario) {
+        this.idUsuario = idUsuario;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    @Override
+    public String toString() {
+        return "Usuario{" +
+                "idUsuario=" + idUsuario +
+                ", nombre='" + nombre + '\'' +
+                '}';
+    }
 }
